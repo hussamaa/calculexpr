1b15d2ebe5249353a14706500c1d7ea517da3462
Hussama Ismail <hussama.ismail@gmail.com>
2017-03-07 22:44:39 +0100

    creating basic maven structure

---
+++ src/main/java/br/eti/hussamaismail/calculexpr/Application.java
@@ -0,0 +1,14 @@
+package br.eti.hussamaismail.calculexpr;
+
+/**
+ * Main class for calculexpr project.
+ *  
+ * @author Hussama Ismail
+ */
+public final class Application {
+
+	public static void main(final String[] args) {
+		
+	}
+
+}
